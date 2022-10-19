399c477a0871b293ba9798eacc1b8bae6c4ff0be
revival0728 <revival0728@gmail.com>
2022-10-19 09:35:31 +0800

    add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export function renderMdToHtml(mdString: string, options: {[key: "autoChangeLine"]: boolean, [key: "withIndent"]: boolean}): {[key: "content"]: string, [key: "data"]: {[key: string]: any}}
